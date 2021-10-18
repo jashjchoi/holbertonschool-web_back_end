e37fe6a13a4c9a2727befc98b62e5f307029b294
jashjchoi <jashjchoi@gmail.com>
2021-10-18 18:30:31 +0000

    Add task 1 file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""type-annotated function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat string str1 and a string str2 as arguments
+    Returns:
+        str: str1 + str2
+    """
+    return str1 + str2

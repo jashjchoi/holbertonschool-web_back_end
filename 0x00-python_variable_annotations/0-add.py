1a378174d5974d6c4c3fe066ab07b308d527d7a2
jashjchoi <jashjchoi@gmail.com>
2021-10-18 18:12:47 +0000

    set up the new project folder and add task 0 file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""type-annotated function"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two float numbers a and b as arguments
+    Returns:
+        float: a + b
+    """
+    return a + b

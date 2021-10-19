da806119ad8f8810403c4709565e5eddf2bf52df
jashjchoi <jashjchoi@gmail.com>
2021-10-19 21:02:53 +0000

    debugging

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Async routine called wait_n that takes in 2 int arguments
     Returns:

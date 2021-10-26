5aabcec1cfbf18157cdaad7ed2eb2bcc7c0fab17
jashjchoi <jashjchoi@gmail.com>
2021-10-26 02:32:35 +0000

    set up the new project folder

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Basic dictionary
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """put new value into cache_data dictionary
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get value of cache_data dictionary
+        """
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]

217eabc5003f9ce0ffb87a37a363afefd2f26cdb
jashjchoi <jashjchoi@gmail.com>
2021-11-02 00:44:22 +0000

    Add task 3 files. Connects to secure db

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,6 +5,8 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
 
 
 PII_FIELDS = ('email', 'phone', 'ssn', 'password', 'name')
@@ -54,3 +56,17 @@ def get_logger() -> logging.Logger:
     handler.setFormatter(formatter)
     user_data.addHandler(handler)
     return user_data
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Connect to secure holberton database to read a users table
+    The database is protected by a username and password
+    that are set as environment variables on the server named
+    """
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
+    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    data = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(user=user, password=password,
+                                   host=host, database=data)

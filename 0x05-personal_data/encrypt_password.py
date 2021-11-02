b64b61add3338e1b60405e86f2f37548dc3cd130
jashjchoi <jashjchoi@gmail.com>
2021-11-02 01:20:42 +0000

    add task 6 file. pw validate function

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,6 +8,16 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """returns a salted hashed password, which is a byte string"""
     return bcrypt.hashpw(
-        password.encode(),
+        password.encode('utf-8'),
         bcrypt.gensalt()
+    )
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """expects 2 arguments and returns a boolean
+    validate that the provided password matches the hashed password
+    """
+    return bcrypt.checkpw(
+        password.encode('utf-8'),
+        hashed_password
     )

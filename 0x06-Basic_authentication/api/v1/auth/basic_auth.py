eb1cca8a3a012187ffc1097e5bf52368a030d703
jashjchoi <jashjchoi@gmail.com>
2021-11-04 21:37:12 +0000

    update function for task 8

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -18,3 +18,18 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self,
+                                           authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string
+        base64_authorization_header
+        """
+        if not authorization_header or \
+           not isinstance(authorization_header, str):
+            return None
+        try:
+            b = base64.b64decode(authorization_header)
+            decoded_code = b.decode("utf-8")
+        except Exception:
+            return None
+        return decoded_code

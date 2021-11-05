2de172472c5e7b113e8a430e071a69fa4a8202e8
jashjchoi <jashjchoi@gmail.com>
2021-11-05 03:16:14 +0000

    undo the changes

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -19,11 +19,7 @@ class Auth:
         if path[-1] != '/':
             path = path + '/'
         if path in excluded_paths:
-            for _path in excluded_paths:
-                if _path.endswith('/') or _path.endswith('*'):
-                    _path = _path[:-1]
-                if _path in path:
-                    return False
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:

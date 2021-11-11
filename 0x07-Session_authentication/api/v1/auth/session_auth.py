fffce44f1956eaf8174841a3e7ad2c4127c19879
jashjchoi <jashjchoi@gmail.com>
2021-11-11 01:07:34 +0000

    update the class and add the new route

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -36,3 +36,20 @@ class SessionAuth(Auth):
             if session_cookie:
                 user_id = self.user_id_for_session_id(session_cookie)
                 return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """deletes the user session / logout:
+        """
+        if not request:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if not user_id:
+            return False
+        try:
+            del self.user_id_by_session_id[session_id]
+        except Exception:
+            pass
+        return True
--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -37,3 +37,14 @@ def login():
     res = jsonify(user.to_json())
     res.set_cookie(SESSION_NAME, session_id)
     return res
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200

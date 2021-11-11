6e3ce8550f1428d48ed761332b410f4c312c4933
jashjchoi <jashjchoi@gmail.com>
2021-11-11 01:54:27 +0000

    undo the changes

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -24,11 +24,10 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
-        session_dict = {
-            "user_id": user_id,
-            "created_at": datetime.now()
+        SessionExpAuth.user_id_by_session_id[session_id] = {
+            'user_id': user_id,
+            'created_at': datetime.now()
         }
-        self.user_id_by_session_id[session_id] = session_dict
         return session_id
 
     def user_id_for_session_id(self, session_id=None):

4cc60698cd3b722c2848f3649b9dc40b8e689182
jashjchoi <jashjchoi@gmail.com>
2021-11-11 01:46:12 +0000

    modified function

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -23,10 +23,11 @@ class SessionExpAuth(SessionAuth):
         session_id = super().create_session(user_id)
         if session_id is None:
             return None
-        SessionExpAuth.user_id_by_session_id[session_id] = {
-            'user_id': user_id,
-            'created_at': datetime.now()
+        session_dict = {
+            "user_id": user_id,
+            "created_at": datetime.now()
         }
+        self.user_id_by_session_id[session_id] = session_dict
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
@@ -34,18 +35,20 @@ class SessionExpAuth(SessionAuth):
         """
         if session_id is None:
             return None
-        session_dict = self.user_id_by_session_id.get(session_id, None)
+        if session_id not in self.user_id_by_session_id.keys():
+            return None
+        session_dict = self.user_id_by_session_id.get(session_id)
         if session_dict is None:
             return None
-        if 'created_at' not in session_dict:
-            return None
+    
         if self.session_duration <= 0:
             return session_dict.get('user_id')
 
         created_at = session_dict.get('created_at')
+        if created_at is None:
+            return None
         session_len = timedelta(seconds=self.session_duration)
         expired_timestamp = created_at + session_len
-
         if expired_timestamp < datetime.now():
             return None
         return session_dict.get('user_id')

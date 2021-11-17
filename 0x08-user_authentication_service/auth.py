b78fd4fa27b849332c64c8d228ceba1058ebb9a1
jashjchoi <jashjchoi@gmail.com>
2021-11-17 08:14:02 +0000

    implement valid login method

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,9 +2,10 @@
 """Auth class"""
 
 from db import DB
-from typing import TypeVar, Union
 from user import User
+from typing import TypeVar, Union
 import bcrypt
+from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
 
 
@@ -27,3 +28,11 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """If pw is valid returns true, otherwise false"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False

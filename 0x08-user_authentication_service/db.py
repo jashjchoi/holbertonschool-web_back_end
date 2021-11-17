be6075e9610dfd23ee05792165e5dab36b2fb60a
jashjchoi <jashjchoi@gmail.com>
2021-11-17 03:15:00 +0000

    debugging

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -5,7 +5,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import Base, User
-from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:

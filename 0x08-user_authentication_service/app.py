ad9e0496ee93ce624ed35afd2bf5a140dcf29ebb
jashjchoi <jashjchoi@gmail.com>
2021-11-18 04:31:09 +0000

    add profile function

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -57,5 +57,18 @@ def logout() -> str:
     return redirect('/', code=302)
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ GET /profile
+    Find the user. If the user exist, respond with a 200 HTTP status
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

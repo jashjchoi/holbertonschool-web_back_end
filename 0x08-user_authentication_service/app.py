7a2e4b291cd86e1af54203c3d58f607f6c1a54c7
jashjchoi <jashjchoi@gmail.com>
2021-11-18 04:35:41 +0000

    implement functions for tasks

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,6 @@
 """Basic Flask app
 """
 from auth import Auth
-from sqlalchemy.orm.exc import NoResultFound
 from flask import Flask, jsonify, request, abort, redirect
 
 AUTH = Auth()
@@ -51,10 +50,11 @@ def logout() -> str:
     If the user does not exist, respond with a 403 HTTP status"""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if not user:
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
         abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect('/', code=302)
 
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
@@ -67,6 +67,37 @@ def profile() -> str:
     if user:
         return jsonify({"email": user.email}), 200
     else:
+        abort(403)
+
+
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """get_reset_password
+    Return:
+       str: message
+    """
+    email = request.form.get('email')
+    user = AUTH.create_session(email)
+    if not user:
+        abort(403)
+    else:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": f"{email}", "reset_token": f"{token}"})
+
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """Reset the pw and update
+    take an email string argument and returns a string
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_psw = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_psw)
+        return jsonify({"email": f"{email}",
+                        "message": "Password updated"}), 200
+    except Exception:
         abort(403)
 
 

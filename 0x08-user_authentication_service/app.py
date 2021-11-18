01fce46067f9e168325ec3bf117f3d44e66b41aa
jashjchoi <jashjchoi@gmail.com>
2021-11-18 04:20:00 +0000

    debugging

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -49,12 +49,16 @@ def logout() -> str:
     If the user exists destroy the session and redirect the user to GET /
     If the user does not exist, respond with a 403 HTTP status"""
     session_id = request.cookies.get('session_id')
+    if session_id is None:
+        return abort(403)
+
     user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        AUTH.destroy_session(user.id)
-        return redirect('/')
-    else:
+
+    if user is None:
         abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect(url_for('basic'))
 
 
 if __name__ == "__main__":

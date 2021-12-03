33f50643252602af49a2c1e84c57e9715cd7a6c6
jashjchoi <jashjchoi@gmail.com>
2021-12-03 04:40:00 +0000

    change the function and procedure name

--- 0x0C-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,22 +1,20 @@
 -- SQL script that creates a stored procedure
--- ComputeAverageWeightedScoreForUsers that computes and
+-- ComputeAverageWeightedScoreForUser that computes and
 -- store the average weighted score for all students.
 -- Procedure ComputeAverageScoreForUser is taking 1 input:
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER //
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
 BEGIN
-    UPDATE users AS User, 
-        (SELECT User.id, SUM(score * weight) / SUM(weight) AS w_avg 
-        FROM users AS User 
-        JOIN corrections as Cor ON User.id=Cor.user_id 
-        JOIN projects AS Pj ON Cor.project_id=Pj.id 
-        GROUP BY User.id)
-    AS W_Average
-    SET User.average_score = W_Average.w_avg
-    WHERE User.id=W_Average.id;
+    DECLARE w_avg FLOAT;
+    SET w_avg = (SELECT SUM(score * weight) / SUM(weight) 
+                        FROM users AS User
+                        JOIN corrections as Cor ON User.id=Cor.user_id 
+                        JOIN projects AS Project ON Cor.project_id=Project.id 
+                        WHERE User.id=user_id);
+    UPDATE users SET average_score = w_avg WHERE id=user_id;
 END
 //
 DELIMITER ;

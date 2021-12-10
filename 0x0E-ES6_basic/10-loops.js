259bf7740ecccafbe823dd60347ecaa7ed54224a
jashjchoi <jashjchoi@gmail.com>
2021-12-10 04:05:01 +0000

    debugging error

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,9 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const aux = array;
-    for (const value of array) {
-      const index = array.indexOf(value);
-      aux[index] = appendString + value;
-    }
-  
-    return aux;
+  const newArray = [];
+  for (const element of array) {
+    newArray.push(`${appendString}${element}`);
   }
+  return newArray;
+}

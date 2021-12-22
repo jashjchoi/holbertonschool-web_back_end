13800957bf00a6e8f6e7167b3b56d8cedc04c17a
jashjchoi <jashjchoi@gmail.com>
2021-12-22 03:03:52 +0000

    add more task files

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+xport default function cleanSet(set, startString) {
+  const output = [];
+
+  if (set instanceof Set === false) return '';
+  if (typeof startString !== 'string') return '';
+  if (startString.length <= 0) return '';
+
+  [...set].forEach((value) => {
+    if (typeof value !== 'string') return;
+    if (value.startsWith(startString)) {
+      output.push(value.substring(startString.length));
+    }
+  });
+  return output.join('-');
+}

a42f58bdafb0ff493b73914d978bf2996ce149f2
jashjchoi <jashjchoi@gmail.com>
2022-01-06 02:18:36 +0000

    debugging

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -5,15 +5,12 @@ const utils = require('./utils')
 
 
 describe('Spies', function () {
-  it('Is the same math', () => {
+  it('Spies output:', () => {
     const spyFunction = sinon.spy(utils, 'calculateNumber');
     const spyConsole = sinon.spy(console, 'log');
-    const apiRequest = sendPaymentRequestToApi(100, 20);
-
 
     expect(spyFunction.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
     expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
-    expect(utils.calculateNumber('SUM', 100, 20)).to.equal(apiRequest);
 
     spyFunction.restore();
     spyConsole.restore();

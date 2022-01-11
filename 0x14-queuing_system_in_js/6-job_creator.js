25c2163ad8311c3d14ab9c3891880154d532d4ca
jashjchoi <jashjchoi@gmail.com>
2022-01-11 05:40:04 +0000

    add task 7 files

--- 0x14-queuing_system_in_js/6-job_creator.js
+++ 0x14-queuing_system_in_js/6-job_creator.js
@@ -1,14 +1,18 @@
-import kue from 'kue';
-
+const kue = require('kue');
 const queue = kue.createQueue();
-const jobData = {
-  phoneNumber: '01073207044',
-  message: 'verify phone number',
-};
-const job = queue.create('push_notification_code', jobData)
-  .save((error) => {
-    if (!error) console.log(`Notification job created: ${job.id}`);
-  });
 
-job.on('complete', () => console.log('Notification job completed'));
-job.on('failed', () => console.log('Notification job failed'));
+const jobData = {
+  phoneNumber: '123456789',
+  message: 'Here is the message',
+}
+const job = queue.create('push_notification_code', jobData).save((error) => {
+  if(!error) console.log(`Notification job created: ${job.id}`);
+});
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
